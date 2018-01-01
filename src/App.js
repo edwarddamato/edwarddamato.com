c1ef2085b194a9cd477bd5d15aa1fe83a889c0bb
Edward D'Amato <info@edwarddamato.com>
2018-01-01 19:43:56 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>edwarddamato</h1>
+          <h2>Watch this space</h2>
+        </div>
+        <a href="https://github.com/edwarddamato">View on Github</a>
+      </div>
+    );
+  }
+}
+
+export default App;
